7b5151b4a87ccdefdc09453e1714b3474dcbf3f4
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2020-11-09 01:39:56 +0530

    if else

--- src/hello.go
+++ src/hello.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, 世界")
+	x := 5
+	y := 6
+	var sum = x + y
+	fmt.Println(sum)
+	if sum > 10 {
+		fmt.Println("ok")
+	} else if sum < 12 {
+		fmt.Println("try 1")
+	} else {
+		fmt.Println("try 2")
+	}
+
+	var a [5]int
+	fmt.Println(a)
 }
